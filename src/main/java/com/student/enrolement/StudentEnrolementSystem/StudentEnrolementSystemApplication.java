08869236050df2cc01dee909642879de9879abc2
imarungupta <imarungupta@gmail.com>
2022-07-10 19:12:25 +0530

    FirstCommit

---
+++ src/main/java/com/student/enrolement/StudentEnrolementSystem/StudentEnrolementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.student.enrolement.StudentEnrolementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentEnrolementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentEnrolementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/student/enrolement/StudentEnrolementSystem/StudentEnrolementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.enrolement.StudentEnrolementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentEnrolementSystemApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
